86d879ff3978c846d1a234d93f4363e77f58693e
marouanekadiri <marouanekadiri@gmail.com>
2020-11-12 19:35:29 +0100

    document code and refactor

--- src/declareBreakpoints.js
+++ src/declareBreakpoints.js
@@ -1,5 +1,6 @@
 import React, {
   forwardRef,
+  useCallback,
   useContext,
   useEffect,
   useRef,
@@ -8,6 +9,13 @@ import React, {
 import { RC_SIZE_PROP_NAME } from "./constants";
 import { useRCContext } from "./RCContext";
 
+/**
+ * 
+ * @param {Array} breakpoints list of function breakpoint (e.g. `[({width}) => width > 500,({width}) => width > 600]`)
+ * @param {Object} size {width, height}
+ * @returns Binary string that for each binary digit 
+ * represents a boolean (0 or 1) based on breakpoint condition against the height and width specified in the second argument
+ */
 const breakpointsToBinaryString = (breakpoints, { height, width }) =>
   breakpoints.reduce(
     (cum, breakpoint) =>
@@ -15,9 +23,23 @@ const breakpointsToBinaryString = (breakpoints, { height, width }) =>
     ""
   );
 
+/**
+ * Get width of the html element (offsetWidth)
+ * @param {HTMLElement} element 
+ */
 const getWidth = (element) => (element ? element.offsetWidth : undefined);
+
+/**
+ * Get height of the html element (offsetHeight)
+ * @param {HTMLElement} element 
+ */
 const getHeight = (element) => (element ? element.offsetHeight : undefined);
 
+/**
+ * Get width and height of html element (offsetWidth and offsetHeight)
+ * @param {HTMLElement} element
+ * @returns e.g. ({ width: 600, height: 700 })
+ */
 const getSize = (element) => {
   return {
     width: getWidth(element),
@@ -25,6 +47,16 @@ const getSize = (element) => {
   };
 };
 
+/**
+ * 
+ * @param  {...Function} breakpoints list of (({width}) => width > 500, ({width}) => width > 600)
+ * @returns Function that takes two parameters `Component` and `lazy` and return a `WrappedComponent`
+ * 
+ * `Component` mainly the component that should re-render
+ * 
+ * `lazy` set this to true if you want your component to mount after 
+ *  the relative container renders first (mainly to avoid a second re-render because of width and height state switch)
+ */
 const declareBreakpoints = (...breakpoints) => (Component, lazy) => {
   // add feature of breakpoint as object
   const WrapperComponent = forwardRef((props, ref) => {
@@ -35,37 +67,32 @@ const declareBreakpoints = (...breakpoints) => (Component, lazy) => {
       breakpointsToBinaryString(breakpoints, getSize(context.getElement()))
     );
 
+    const reevaluateBreakpoints = useCallback(element => {
+      const newbreakpointValue = breakpointsToBinaryString(
+        breakpoints,
+        getSize(element)
+      );
+      if (breakpointValue.current !== newbreakpointValue) {
+        breakpointValue.current = newbreakpointValue;
+        setSize(getSize(element));
+      }
+    }, [])
+
     useEffect(() => {
       if (context) {
         const element = context.getElement();
-        const newbreakpointValue = breakpointsToBinaryString(
-          breakpoints,
-          getSize(element)
-        );
-        if (breakpointValue.current !== newbreakpointValue) {
-          breakpointValue.current = newbreakpointValue;
-          setSize(getSize(element));
-        }
+        reevaluateBreakpoints(element);
       }
     }, [context]);
 
     useEffect(() => {
       if (context) {
-        const listenToResize = (event) => {
-          const width = event[0].target.offsetWidth;
-          const height = event[0].target.offsetHeight;
-          const newbreakpointValue = breakpointsToBinaryString(breakpoints, {
-            width,
-            height,
-          });
-          if (breakpointValue.current !== newbreakpointValue) {
-            breakpointValue.current = newbreakpointValue;
-            setSize({ width, height });
-          }
+        const observeResize = (event) => {
+          reevaluateBreakpoints(event[0].target)
         };
-        context.addListener(listenToResize);
+        context.addListener(observeResize);
         return () => {
-          context.removeListener(listenToResize);
+          context.removeListener(observeResize);
         };
       }
     }, [context]);
